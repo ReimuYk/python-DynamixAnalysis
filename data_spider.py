74c118e54cde0078c95c9dcfa71e4706fe7e58bd
ReimuYk <513639514@qq.com>
2019-02-08 19:26:23 +0800

    spider all

--- data_spider.py
+++ data_spider.py
@@ -1,5 +1,7 @@
 import requests
 import re
+import pickle as pkl
+import os
 
 ##data = requests.get(r"https://dynamixc4cat.fandom.com/wiki/Stardust")
 ##data = data.content
@@ -17,6 +19,8 @@ def parse(url):
     ret["composer"] = re.findall(r"/>(.+?)</td",lines[0])[0]
     tds = re.findall(r'center">(.+?)</td',lines[2])
     ret["No"] = tds[0]
+    while len(ret["No"])<3:
+        ret["No"] = "0" + ret["No"]
     ret["update"] = tds[1]
     ret["unlock"] = tds[2].replace(r"<br />","")
     ret["pic"] = re.findall(r'href="(.+?)"',lines[3])[0]
@@ -32,5 +36,48 @@ def parse(url):
     ret["rightside"] = tds[4]
     return ret
 
-r = parse(testurl)
-print(r)
+def get_songlist():
+    url = r"https://dynamixc4cat.fandom.com/wiki/Songs_by_BPM"
+    data = requests.get(url)
+    data = data.content.decode("UTF-8")
+    txt = data.replace("\n","")
+    txt = re.findall(r"wikitable(.+?)</table",txt)[0]
+    songs = re.findall(r"tr>(.+?)</tr",txt)
+    songs = songs[2:]
+    for i in range(len(songs)):
+        songs[i] = re.findall(r'href="(.+?)"',songs[i])[0]
+        songs[i] = r"https://dynamixc4cat.fandom.com" + songs[i]
+    return songs # urllist of songs
+
+def download_songdata():
+    for url in get_songlist():
+        info = parse(url)
+        print("No.%s download"%info["No"],end="\t")
+        try:
+            f = open("./song_data/%s.pkl"%info["No"],"wb")
+            pkl.dump(info,f)
+            f.close()
+            print(info["title"])
+        except:
+            print("error in save")
+            
+def download_songpic():
+    for filename in os.listdir("./song_data"):
+        outname = filename.replace("pkl","png")
+        if not os.path.exists("./song_data/%s"%outname):
+            f = open("./song_data/%s"%filename,"rb")
+            data = pkl.load(f)
+            f.close()
+            print(data["pic"])
+            pic = requests.get(data["pic"])
+            f = open("./song_data/%s"%outname,"wb")
+            f.write(pic.content)
+            f.close()
+
+download_songpic()
+            
+    
+
+
+##r = parse(testurl)
+##print(r)
